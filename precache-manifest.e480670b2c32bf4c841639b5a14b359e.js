3638c3db2a3dea01f80ff8e7441f06aa6874a195
alisherakb <alisher286@gmail.com>
2019-11-20 03:00:17 +0400

    Updates

---
+++ precache-manifest.e480670b2c32bf4c841639b5a14b359e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f517986f7ddbca9c861e107edd89b279",
+    "url": "/pastieweb/index.html"
+  },
+  {
+    "revision": "e879ebd5badf50866b94",
+    "url": "/pastieweb/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c5d9e1b3ce6154109da9",
+    "url": "/pastieweb/static/js/2.854d310f.chunk.js"
+  },
+  {
+    "revision": "e879ebd5badf50866b94",
+    "url": "/pastieweb/static/js/main.13f23302.chunk.js"
+  },
+  {
+    "revision": "43b5105e27a79e42a7ac",
+    "url": "/pastieweb/static/js/runtime-main.d3481459.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pastieweb/static/media/logo.25bf045c.svg"
+  }
+]);
