ed53543ab1282f7fa2bd1a025250bc906f0f68df
alisherakb <alisher286@gmail.com>
2019-11-20 12:13:00 +0400

    Updates

---
+++ precache-manifest.9467aa982166771e9aedb9c6c0fe490b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f93d36adfc97bcf8f409997f8f74cbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "077992e7db2e52052f87",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c5d9e1b3ce6154109da9",
+    "url": "/static/js/2.854d310f.chunk.js"
+  },
+  {
+    "revision": "077992e7db2e52052f87",
+    "url": "/static/js/main.e2ecf187.chunk.js"
+  },
+  {
+    "revision": "36b352a959b0f360e7c5",
+    "url": "/static/js/runtime-main.5c157bab.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
